077b49250753e839ae9d6bc017656c3bbf48e142
Wh1spr <wh1spr.code@gmail.com>
2017-08-04 11:40:49 +0200

    clean command
    should’ve already been here.

--- src/main/java/wh1spr/olympians/zeus/Zeus.java
+++ src/main/java/wh1spr/olympians/zeus/Zeus.java
@@ -36,6 +36,9 @@ public class Zeus implements Bot{
 		adminRegistry.registerCommand("grantimmunity", new ImmunityGrantCommand(), "gi");
 		adminRegistry.registerCommand("revokeimmunity", new ImmunityRevokeCommand(), "ri");
 		
+		// Channel Control
+		adminRegistry.registerCommand("clean", new CleanCommand());
+		
 		// Extra
 		adminRegistry.registerCommand("eval", new EvalCommand());
 		
